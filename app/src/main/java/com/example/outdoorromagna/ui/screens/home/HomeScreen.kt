6d2e90c4775c0876e708e5abfab315770ec2387f
Riccardo Frascio <riccardo.frascio@studio.unibo.it>
2024-05-16 18:21:21 +0200

    grafica searchbar

--- app/src/main/java/com/example/outdoorromagna/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/outdoorromagna/ui/screens/home/HomeScreen.kt
@@ -2,35 +2,48 @@ package com.example.outdoorromagna.ui.screens.home
 
 import android.Manifest
 import android.content.Context
+import android.content.res.Resources.Theme
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.GpsFixed
 import androidx.compose.material.icons.outlined.Layers
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
@@ -41,6 +54,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -215,31 +230,55 @@ fun HomeScreen(
                                 }
                             }
                         if (state.showSearchBar) {
-                            SearchBar { query ->
-                                performSearch(query = query, context = context) { results ->
-                                    if (results.isNotEmpty()) {
-                                        placeLocations = results
-                                        Log.d("tag", results.toString())
-                                        center = results.first().latLng //forse da togliere
-                                        cameraPositionState.move(
-                                            CameraUpdateFactory.newLatLngZoom(
-                                                center,
-                                                12f
-                                            )
-                                        ) //forse da togliere
+                            Column {
+                                Row {
+                                    SearchBar(actions = actions, onQueryChanged =  { query ->
+                                            performSearch(query = query, context = context) { results ->
+                                                if (results.isNotEmpty()) {
+                                                    placeLocations = results
+                                                    Log.d("tag", results.toString())
+
+                                                }
+                                            }
+                                        }
+                                    )
+                                }
+                                if(placeLocations.isNotEmpty()) {
+                                    placeLocations.forEach { place ->
+                                        Row(
+                                            modifier = Modifier
+                                                .padding(vertical = 0.dp)
+                                                .fillMaxWidth()
+                                                .background(MaterialTheme.colorScheme.background)
+                                                .border(
+                                                    BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground),
+                                                    shape = RoundedCornerShape(0.dp,0.dp,4.dp,4.dp)
+                                                )
+                                        ) {
+                                            TextButton(
+                                                onClick = {
+                                                    center = place.latLng
+                                                    cameraPositionState
+                                                        .move(CameraUpdateFactory.newLatLngZoom(center,12f))
+                                                    actions.setShowSearchBar(false)
+                                                    placeLocations = listOf()
+                                                },
+                                                colors = ButtonDefaults.textButtonColors(
+                                                    containerColor = MaterialTheme.colorScheme.background,
+                                                    contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                                                ),
+                                                modifier = Modifier
+                                                    .padding(vertical = 0.dp)
+                                                    .fillMaxWidth(),
+                                                shape = RectangleShape,
+                                                contentPadding = PaddingValues(vertical = 0.dp),
+                                            ) {
+                                                Text(text = place.name)
+                                            }
+                                        }
                                     }
                                 }
                             }
-                            /*if(placeLocations.isNotEmpty()) {
-                            DropdownMenu(
-                                modifier = Modifier.fillMaxWidth(),
-                                expanded = true,
-                                onDismissRequest = { /*actions.setShowSearchBar(false)*/ }) {
-                                placeLocations.forEach { place ->
-                                    DropdownMenuItem(text = { place.name }, onClick = { /*TODO*/ })
-                                }
-                            }
-                        }*/
                         }
 
 
@@ -371,7 +410,7 @@ fun rememberPermission(
 }
 
 @Composable
-private fun SearchBar(onQueryChanged: (String) -> Unit) {
+private fun SearchBar(onQueryChanged: (String) -> Unit, actions: HomeScreenActions) {
     var text by remember { mutableStateOf("") }
 
     TextField(
@@ -381,10 +420,15 @@ private fun SearchBar(onQueryChanged: (String) -> Unit) {
             onQueryChanged(it)
         },
         label = { Text("Cerca luogo") },
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .padding(0.dp)
+            .fillMaxWidth(),
         singleLine = true,
+        shape = RectangleShape,
         trailingIcon = {
-            Icon(Icons.Default.Search, contentDescription = "Search")
+            IconButton(onClick = { actions.setShowSearchBar(false) }) {
+                Icon(Icons.Outlined.Close, contentDescription = "Chiudi")
+            }
         }
     )
 }
